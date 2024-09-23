fa113d06a347b7461478aabb5fd3577497549161
salgozino <salgozino@gmail.com>
2024-09-23 09:58:07 -0300

    fix: missing pings tx log

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import logging
 
 from contract import Contract
 
+PING_PONG_ADDRESS = "0xA7F42ff7433cB268dD7D59be62b00c30dEd28d3D"
+
 
 def setup_logger(log_file="logs.log", level=logging.INFO):
     # Configure log file
@@ -21,13 +23,7 @@ def setup_logger(log_file="logs.log", level=logging.INFO):
     return logger
 
 
-def main(candidate_bot: str, candidate_starting_block: int, logger: logging.Logger):
-    logger.info(
-        f"Starting to review pongs for candidate: {candidate_bot} since block: {candidate_starting_block}"
-    )
-    contract_address = "0xA7F42ff7433cB268dD7D59be62b00c30dEd28d3D"
-    ping_pong_contract = Contract(contract_address, "PingPongABI.json", logger)
-
+def getEvents(ping_pong_contract, starting_block, candidate_bot):
     all_pings = ping_pong_contract.get_all_pings(candidate_starting_block)
     # logger.info(f"Ping example: {all_pings[0]}")
     all_pongs = ping_pong_contract.get_all_pongs(
@@ -35,6 +31,18 @@ def main(candidate_bot: str, candidate_starting_block: int, logger: logging.Logg
     )
     # logger.info(f"Pong example: {all_pongs[0]}")
     logger.info(f"Pings: {len(all_pings)} | Pongs: {len(all_pongs)}")
+    return all_pings, all_pongs
+
+
+def main(candidate_bot: str, candidate_starting_block: int, logger: logging.Logger):
+    logger.info(
+        f"Starting to review pongs for candidate: {candidate_bot} since block: {candidate_starting_block}"
+    )
+    ping_pong_contract = Contract(PING_PONG_ADDRESS, "PingPongABI.json", logger)
+
+    all_pings, all_pongs = getEvents(
+        ping_pong_contract, candidate_starting_block, candidate_bot
+    )
 
     # Let's check the ping arg in pong events agains the ping txs.
     pings_hash_in_pongs = set([pong["args"]["txHash"] for pong in all_pongs])
@@ -47,7 +55,7 @@ def main(candidate_bot: str, candidate_starting_block: int, logger: logging.Logg
         )
 
     # Duplicated pongs check.
-    if len(all_pongs) != len(pings_hash_in_pongs):
+    if len(set(pings_hash_in_pongs)) != len(pings_hash_in_pongs):
         logger.error("There are duplicated pingHash in pongs")
     else:
         logger.info("There are not duplicated pingHash in pongs")
@@ -59,15 +67,11 @@ def main(candidate_bot: str, candidate_starting_block: int, logger: logging.Logg
     logger.info("All pongs events has a valid ping tx")
 
     # Missing pings
-    if len(pings_hash_in_pongs) < len(all_pings):
+    if len(pings_hash_in_pongs) < len(ping_txs):
         logger.error(
-            f"There are {len(pings_hash_in_pongs) / len(all_pings) * 100:.2f}% missing ping txs"
+            f"There are {(1 - len(pings_hash_in_pongs) / len(ping_txs)) * 100:.2f}% missing ping txs"
         )
-        missing_pings = [
-            ping.transactionHash
-            for ping in all_pings
-            if not ping in pings_hash_in_pongs
-        ]
+        missing_pings = [ping for ping in ping_txs if ping not in pings_hash_in_pongs]
         logger.error(f"Missing pings: {missing_pings}")
 
 
